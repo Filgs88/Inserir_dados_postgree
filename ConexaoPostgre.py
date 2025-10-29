cc92d920180722de91ff35c76a4fd4610ea56694
Filgs88 <125389062+Filgs88@users.noreply.github.com>
2025-10-29 08:04:17 -0400

    Update ConexaoPostgre.py

--- ConexaoPostgre.py
+++ ConexaoPostgre.py
@@ -7,7 +7,7 @@ class ConexaoPostgre:
         Cria e retorna uma engine SQLAlchemy para o banco PostgreSQL.
         """
         engine = create_engine(
-            "postgresql+psycopg2://postgres:202323@192.168.68.168:5432/postgres",
+            "string-connection",
             pool_pre_ping=True  # Verifica se a conexão ainda é válida
         )
         return engine
